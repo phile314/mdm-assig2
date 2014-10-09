c4f5301d55614d4d65a0472553b4c3eb681221b7
Philipp Hausmann <ph_git@314.ch>
2014-10-09 16:39:25 +0200

    Added data

--- src/gm.r
+++ src/gm.r
@@ -1,8 +1,28 @@
 
-gm.restart <- function() {
+gm.restart <- function(nstart, prob, seed, obs, forward, backward, score) {
+  set.seed(seed)
+  best <- NULL
+  for (i in (1:nstart)) {
+    # generate random graph
+    gr <- gm.randGraph(prob)
 
+    res <- gm.search(obs, gr, forward, backward, score)
+
+    # is smaller score better?
+    if(best == NULL || best$score > res$score) {
+      best <- res
+    }
+  }
+
+  # how do we return the "call" (requested by Ad)?
+
+  return(list(call = match.call(), best = best))
 }
 
 gm.search <- function() {
+
+}
+
+gm.randGraph <- function(prob) {
 
 }
