cc3db1876d1874afc554f591fec58314bd0cad9b
Philipp Hausmann <ph_git@314.ch>
2014-10-15 15:16:56 +0200

    Skeleton

--- src/gm.r
+++ src/gm.r
@@ -4,7 +4,7 @@ gm.restart <- function(nstart, prob, seed, obs, forward, backward, score) {
   best <- NULL
   for (i in (1:nstart)) {
     # generate random graph
-    gr <- gm.randGraph(prob)
+    gr <- gm.randGraph(prob, length(dim(obs)))
 
     res <- gm.search(obs, gr, forward, backward, score)
 
@@ -23,6 +23,20 @@ gm.search <- function() {
 
 }
 
-gm.randGraph <- function(prob) {
+gm.randGraph <- function(prob, nNodes) {
+  m <- matrix(0,nNodes,nNodes)
+  # TODO update graph
+  # TODO explain why we are setting the diagonal to zero
+  # (probably because it would mean that a node is independent
+  #  from itself, which doesn't sound sensible)
+  for (i in (1:nNodes)) {
+    s <- sample((0:1), (i - 1), c(prob - 1, prob))
+    # fill upper half, set everything else to zero
+    m[i,] <- c(s, rep(0, nNodes - i + 1)
+  }
+  for (i in (1:nNodes)) {
+    m[i,] <- c(m[i,(1:i)], m
+  }
 
+  return(m)
 }
