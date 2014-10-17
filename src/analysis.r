530e86d7901c2dada2de2ce86ca67bbb8fdf8f9d
Marco Vassena <m.vassena@students.uu.nl>
2014-10-17 20:50:10 +0200

    Added functins to print graphs and cliques tables
      See answer c

---
+++ src/analysis.r
@@ -0,0 +1,26 @@
+source("gm.r")
+
+rhc.dat <- read.csv('../data/rhc-small.txt')
+
+clique.table <- function(graphmodel){
+  fmt <- function(set) paste0("{", paste(lapply(set, paste), collapse=", "), "}")
+  cliques <- unique(lapply(graphmodel$cliques, sort))
+  formatted <- lapply(cliques, fmt)
+  clique.df <- as.data.frame(I(formatted))
+  colnames(clique.df) <- "cliques"
+  return(clique.df)
+}
+
+answer.c <- function() gm.search(table(rhc.dat), matrix(0, 10, 10), forward=TRUE,
+                                 backward=TRUE, score="bic")
+
+# Returns an igraph object from the list of its cliques
+from.cliques <- function(cliques){
+  mkFullGraph <- function(clique){
+    g <- graph.full(length(clique))
+    V(g)$name <- paste(clique)
+    return(g)
+  }
+  graphs <- lapply(cliques, mkFullGraph)
+  return(graph.union(graphs))
+}
